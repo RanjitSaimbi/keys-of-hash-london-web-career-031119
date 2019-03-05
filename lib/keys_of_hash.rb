9c495abae021842359802abb53d66fdf79c76fd9
Ranjit Saimbi <ranjit.saimbi@gmail.com>
2019-03-05 15:46:11 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -4,7 +4,6 @@ class Hash
   def keys_of(*arguments)
    
   end
-  binding.pry
 end
 
 # {"sugar glider"=>"Australia","aye-aye"=> "Madagascar","red-footed tortoise"=>"Panama","kangaroo"=> "Australia","tomato frog"=>"Madagascar","koala"=>"Australia"}
