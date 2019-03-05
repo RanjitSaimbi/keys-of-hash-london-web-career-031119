5ea1a524bbd90f9f9bc6c073ba78f4b684ac6c3e
Ranjit Saimbi <ranjit.saimbi@gmail.com>
2019-03-05 15:52:05 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -2,12 +2,14 @@ require 'pry'
 
 class Hash
   def keys_of(*arguments)
+   array = []
    self.values.map do |animal_key, country| 
     if arguments == country
-      animal_key
+      array << animal_key
     end 
    end 
   end
+  array
 end
 
 # {"sugar glider"=>"Australia","aye-aye"=> "Madagascar","red-footed tortoise"=>"Panama","kangaroo"=> "Australia","tomato frog"=>"Madagascar","koala"=>"Australia"}
